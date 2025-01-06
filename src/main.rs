44429b4a41fafd12784e3955a1758d0dfd5cd2af
Vitaliy Alifanov <alifanov.vitalik@yandex.ru>
2025-01-06 15:21:07 +0300

    add player movement

--- src/main.rs
+++ src/main.rs
@@ -39,13 +39,15 @@ fn main() {
                 .load_collection::<GameAssets>(),
         )
         .add_systems(Startup, (spawn_2d_camera,))
+        .add_systems(OnExit(AppState::AssetLoading), spawn_player)
+        .add_systems(Update, move_player.run_if(in_state(AppState::Main)))
         .run();
 }
 
 #[derive(AssetCollection, Resource)]
 struct GameAssets {
     #[asset(path = "art/ball.png")]
-    _ball: Handle<Image>,
+    ball: Handle<Image>,
 }
 
 #[derive(States, Clone, Copy, Eq, PartialEq, Debug, Hash, Default, Reflect)]
@@ -55,6 +57,53 @@ enum AppState {
     Main,
 }
 
+#[derive(Component)]
+pub struct Player {
+    speed: f32,
+}
+
+impl Default for Player {
+    fn default() -> Player {
+        Player { speed: 500. }
+    }
+}
+
 fn spawn_2d_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle { ..default() });
+}
+
+fn spawn_player(mut commands: Commands, game_assets: Res<GameAssets>) {
+    commands.spawn((
+        Name::new("Player"),
+        Player::default(),
+        SpriteBundle {
+            texture: game_assets.ball.clone(),
+            ..default()
+        },
+    ));
+}
+
+fn move_player(
+    mut player_q: Query<(&mut Transform, &Player)>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, player) in player_q.iter_mut() {
+        let mut direction = Vec2::ZERO;
+        if keyboard.pressed(KeyCode::KeyW) {
+            direction.y += 1.;
+        }
+        if keyboard.pressed(KeyCode::KeyS) {
+            direction.y -= 1.;
+        }
+        if keyboard.pressed(KeyCode::KeyD) {
+            direction.x += 1.;
+        }
+        if keyboard.pressed(KeyCode::KeyA) {
+            direction.x -= 1.;
+        }
+
+        let direction = direction.normalize_or_zero();
+        transform.translation += direction.extend(0.) * player.speed * time.delta_seconds();
+    }
 }
